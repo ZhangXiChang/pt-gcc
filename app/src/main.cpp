da982ff9b3616101dca2ae0c589c1381cc324560
张喜昌 <68778073+ZhangXiChang@users.noreply.github.com>
2023-10-24 15:02:12 +0800

    简化主函数

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,31 +1,4 @@
-#include <iostream>
-#include <memory>
-
-class App
+int main()
 {
-private:
-public:
-    App()
-    {
-    }
-    ~App()
-    {
-    }
-
-    using Ptr = std::shared_ptr<App>;
-    static Ptr MakePtr() { return std::make_shared<App>(); }
-};
-int main(int argc, char **argv)
-{
-    App::Ptr app;
-    try
-    {
-        app = App::MakePtr();
-        return EXIT_SUCCESS;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-        return EXIT_FAILURE;
-    }
+    return 0;
 }
